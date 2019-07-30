7d9878b6ebc3ee09c288997e6f8fd013fed9a5f7
Williamreay <48968264+Williamreay@users.noreply.github.com>
2019-07-30 15:08:08 +1000

    Update Circular_manhattan.R

--- Circular_manhattan.R
+++ Circular_manhattan.R
@@ -9,11 +9,8 @@ library("CMplot")
 #Import data for first circos
 library(readxl)
 Circos_1 <- read_excel("~/Desktop/Pairwise_SZ_2018_meta/Figures/Circos_1.xlsx")
-Circos_2 <- read_excel("~/Desktop/Pairwise_SZ_2018_meta/Figures/Circos_2.xlsx")
 
-Plot_1 <- CMplot(Circos_1,plot.type="c",r=0.4,col=c("dodgerblue4","deepskyblue"), chr.labels=paste(c(1:22),sep=""),threshold=2.7e-6,cir.chr.h=1.5,amplify=TRUE,threshold.lty=2,threshold.col="red",signal.line=1,signal.col=c("red"),chr.den.col="grey",bin.size=1e6,outward=TRUE,file="tiff",memo="",dpi=500,file.output=TRUE,verbose=TRUE, LOG10 = TRUE, cir.band = 2, ylim = c(0,12))
+Plot_1 <- CMplot(Circos,plot.type="c",r=0.4,col=c("dodgerblue4","deepskyblue"), chr.labels=paste(c(1:22),sep=""),threshold=2.7e-6,cir.chr.h=1.5,amplify=TRUE,threshold.lty=2,threshold.col="red",signal.line=1,signal.col=c("red"),chr.den.col="grey",bin.size=1e6,outward=TRUE,file="tiff",memo="",dpi=500,file.output=TRUE,verbose=TRUE, LOG10 = TRUE, cir.band = 2, ylim = c(0,12))
 
-
-Plot_2 <- CMplot(Circos_2,plot.type="c",r=0.4,col=c("dodgerblue4","deepskyblue"),chr.labels=paste(c(1:22),sep=""),threshold=2.7e-6,cir.chr.h=1.5,amplify=TRUE,threshold.lty=2,threshold.col="red",signal.line=1,signal.col=c("red"),chr.den.col="grey",bin.size=1e6,outward=TRUE,file="tiff",memo="",dpi=500,file.output=TRUE,verbose=TRUE, LOG10 = TRUE, cir.band = 2, ylim =c (0,12))
 
 
